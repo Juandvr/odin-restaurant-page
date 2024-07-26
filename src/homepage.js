2810bc7737968abddc4a990bf6c3f7ebb27ac0bf
Juandvr <villabonajuan203@gmail.com>
2024-07-26 15:58:42 -0500

    create function to generate the homepage

--- src/homepage.js
+++ src/homepage.js
@@ -2,12 +2,16 @@ function homeLoad() {
     const content = document.getElementById('content');
 
     const main = document.createElement('div');
-    main.innerHTML = '<h1>Welcome to the bakery</h1>';
+    main.innerHTML = '<h1>Welcome to the restaurant</h1>';
     content.appendChild(main);
 
     const section = document.createElement('div');
-    section.innerHTML = '<h3>Schedule</h3><p>every weekday: 8:00am - 4:00pm</p>';
+    section.innerHTML = '<h3>Special dishes</h3><img alt="dish#1"><img alt="dish#2"><img alt="dish#3"><button>See menu</button>';
     content.appendChild(section);
+
+    const location = document.createElement('div');
+    location.innerHTML = '<h4>Av. bread 234</h4>';
+    content.appendChild(location);
 }
 
 export { homeLoad };
