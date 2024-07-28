51f8426a95ccd1f431739c177ab0ef67c37be786
Juandvr <villabonajuan203@gmail.com>
2024-07-28 01:22:55 -0500

    add style to the first part of the homepage

--- src/homepage.js
+++ src/homepage.js
@@ -2,14 +2,16 @@ function homeLoad() {
     const content = document.getElementById('content');
 
     const main = document.createElement('div');
+    main.setAttribute('id', 'main');
     main.innerHTML = `
-        <h1>The best food for you</h1>
-        <p>Have a great time here</p>
-        <button>See menu<button>
+        <h1>The best food <br>for you</h1>
+        <p>Discover the most delicious <br> dishes and spend <br> a great time here</p>
+        <button>Explore Menu</button>
     `;
     content.appendChild(main);
 
     const menu = document.createElement('div');
+    menu.setAttribute('id', 'menu');
     menu.innerHTML = `
         <h3>Popular dishes</h3>
         <div>
@@ -22,6 +24,7 @@ function homeLoad() {
     content.appendChild(menu);
 
     const about = document.createElement('div');
+    about.setAttribute('id', 'about');
     about.innerHTML = `
         <img alt="map">
         <h2>Trust in us</h2>
